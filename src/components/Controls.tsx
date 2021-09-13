9772c5b54f83f4b0c6c27e3f0d270ac93956a299
Peter Lebedev <LPeter@mail.ru>
2021-09-13 13:26:38 +0300

    styllize borders

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -14,7 +14,7 @@ export default function Controls({ active, setActive }: IControlProps) {
   return (
     <div className="container buttons">
       <div className="row">
-        <div className="col btn-group" role="group" aria-label="select price or speed">
+        <div className="col-12 btn-group" role="group" aria-label="select price or speed">
 
           <button type="button"
             className={`btn ${active === 'price' ? 'btn-primary' : 'outline'}`}
