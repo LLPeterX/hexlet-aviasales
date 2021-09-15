40b9561b2eb8ca6f79fabf4c3e8af38b03c4d3c2
Peter Lebedev <LPeter@mail.ru>
2021-09-15 14:16:05 +0300

    group by carrier

--- src/hooks/useGetTickets.ts
+++ src/hooks/useGetTickets.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { ITicket } from '../types/ITicket'
+import _ from 'lodash';
 
 /* 
 Хук получения данных с сервера
@@ -13,29 +14,43 @@ type IReturnType = [
 
 export default function useGetTickets(): IReturnType {
   const [allTickets, setAllTickets] = useState<ITicket[]>([]);
-  //const [searchId, setSearchId] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
   async function fetchTickets() {
     // get searchId
+    setIsLoading(true);
     const resp1 = await fetch('http://localhost:3000/search.json');
-    const data1 = await resp1.json();
-    //setSearchId(data1.searchId);
+    const { searchId } = await resp1.json();
+
+    let stop: boolean = false;
+    let tickets: ITicket[] = [];
+
 
     // get main data from server
     // TODO: сделать цикл do ... while(!stop) с добавлением данных в массив allTickets[]
-    const resp2 = await fetch(`http://localhost:3000/tickets2.json?searchId=${data1.searchId}`);
-    //debugger;
-    const { tickets, stop } = await resp2.json();
-    setAllTickets(tickets);
+    debugger;
+    do {
+      const resp2 = await fetch(`http://localhost:3000/tickets1.json?searchId=${searchId}`);
+      if (!resp2.ok) {
+        continue;
+      }
+      const data = await resp2.json();
+      let chunk: ITicket[] = data.tickets;
+      stop = data.stop;
+      tickets = tickets.concat(chunk);
+    } while (!stop);
 
     // finally
+    setAllTickets(tickets);
     setIsLoading(false);
+    // group by
+    const grouped = _.groupBy(tickets, (item) => item.carrier);
+    console.log('gr = ', grouped);
+
   } // fetchTickets
 
 
   useEffect(() => {
-    setIsLoading(true);
     if (!allTickets.length) {
       fetchTickets();
     }
